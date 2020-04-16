03c29c26fb5ae2a0dc36d287fcf86d2114886daf
Roman Trapeznikov <roma190505@yandex.ru>
2020-04-16 08:47:44 +0300

    add exceptions

---
+++ exceptions.py
@@ -0,0 +1,25 @@
+class BNetException(Exception):
+    pass
+
+
+class LocalApiError(BNetException):
+    pass
+
+
+class LocalApi:
+    class DuplicateError(LocalApiError):
+        def __init__(self, *args, duptype):
+            super().__init__(*args)
+            self.duptype = duptype
+
+    class PasswordError(LocalApiError):
+        pass
+
+    class InvalidCall(LocalApiError):
+        pass
+
+    class NoUserError(LocalApiError):
+        pass
+
+    class InvalidPasswordError(LocalApiError):
+        pass
